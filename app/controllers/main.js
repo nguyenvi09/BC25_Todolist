168d8c098e7e33099066b77e91fd9d7d9cda2d04
nguyenvi09 <nguyentanvi19942010@gmail.com>
2022-05-09 21:26:41 +0700

    up src

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -5,11 +5,28 @@ const service = new Services();
 
 const getEle = id => document.getElementById(id);
 
+//renderHTML 
+const renderHTML = data => {
+    const content = data.reduce((contentHTML, item) => {
+        return (contentHTML += `
+            <li>
+                ${item.activity}
+                <div>
+                    <button class="btn-delete"><i class="fa-solid fa-trash-can"></i></button>
+                    <button class ="btn-check"><i class="fa-solid fa-circle-check"></i></button>
+                </div>    
+            </li>
+        `)
+    }, "");
+
+    getEle("todo").innerHTML = content;
+};
+
 const getTodoList = () => {
     service
         .fetchData()
         .then(result => {
-            console.log(result.data);
+            renderHTML(result.data);
         })
         .catch(error => {
             console.log(error);
