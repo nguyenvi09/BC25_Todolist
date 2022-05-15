ac078f33d9d1172b4d290cf462ed12b418ae8f4d
nguyenvi09 <nguyentanvi19942010@gmail.com>
2022-05-15 11:03:32 +0700

    fix ser.js

--- app/models/Services.js
+++ app/models/Services.js
@@ -8,41 +8,4 @@ export default class Services {
       data,
     });
   }
-
-  //   fetchData() {
-  //     return axios({
-  //       url: "https://626614c8dbee37aff9abd41e.mockapi.io/api/todolist",
-  //       method: "GET",
-  //     });
-  //   }
-
-  //   addTask(task) {
-  //     return axios({
-  //       url: "https://626614c8dbee37aff9abd41e.mockapi.io/api/todolist",
-  //       method: "POST",
-  //       data: task,
-  //     });
-  //   }
-
-  //   deleteTask(id) {
-  //     return axios({
-  //       url: `https://626614c8dbee37aff9abd41e.mockapi.io/api/todolist/${id}`,
-  //       method: "DELETE",
-  //     });
-  //   }
-
-  getTaskById(id) {
-    return axios({
-      url: `https://626614c8dbee37aff9abd41e.mockapi.io/api/todolist/${id}`,
-      method: "GET",
-    });
-  }
-
-  updateTask(task) {
-    return axios({
-      url: `https://626614c8dbee37aff9abd41e.mockapi.io/api/todolist/${task.id}`,
-      method: "PUT",
-      data: task,
-    });
-  }
 }
